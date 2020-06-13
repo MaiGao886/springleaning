f2244a83bed61fa6adfa0dde143f0be703c6999c
MaiGao886 <1309473883@qq.com>
2020-06-13 19:11:34 +0800

    添加yml配置文件

--- springbootlearning/src/main/java/com/xiagao/controller/HelloController.java
+++ springbootlearning/src/main/java/com/xiagao/controller/HelloController.java
@@ -1,13 +1,18 @@
 package com.xiagao.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Value(value = "${my.name}")
+    private String name;
+    @Value("${my.message}")
+    private String message;
 
     @RequestMapping("/hello")
-    public String hello(){
-        return "HelloWorld";
+    public String hello() {
+        return name + "," + message;
     }
 }
